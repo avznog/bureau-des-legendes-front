15aed03c83d82693a0dc818f9f1cdd51238b0d81
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-05-19 20:09:18 +0200

    ⚗️ add env var

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV == "production" ? 'https://api.bureau-des-legendes.juniorisep.com' : 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_BASE_URL
 });
