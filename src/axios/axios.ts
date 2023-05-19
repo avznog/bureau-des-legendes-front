22f9978da0ab53ba0090ca0ec2ecef20dd8f59f5
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-05-19 19:41:40 +0200

    🌐 fixed cors

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.NODE_ENV == "production" ? 'https://api.bureau-des-legendes.juniorisep.com' : 'http://localhost:8000',
-  withCredentials: true
 });
