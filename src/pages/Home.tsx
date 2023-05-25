67df725fdc0703f389fcf8312b10bb3cc7286eb0
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2023-05-25 20:35:51 +0200

    cool home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,32 @@
 import { Button, Card, CardActions, CardContent, Container, Typography } from "@mui/material";
 import axios from "axios";
-import { useEffect } from "react";
-import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
+import { useEffect, useState } from "react";
+import { useAuthUser } from "react-auth-kit";
+import { FormType } from "../constants/form.type";
+import { Form } from "../models/form.model";
 
 export function Home() {
+  const auth = useAuthUser();
+  const [forms, setForms] = useState([]);
+
   useEffect(() => {
-    axios.get(`http://localhost:8000/forms/team/1`).then((response) => console.log(response.data)).catch((error) => console.log(error));
-  })
+    axios.get(`http://localhost:8000/forms/team/${auth()?.person.person.team.id}`).then((response) => setForms(response.data)).catch((error) => console.log(error));
+  }, [setForms])
     
   return (
   <Container>
-        <Card className="mt-5 mb-5 bg-gradient-to-r from-blue-300 to-blue-700">
-          <CardContent>
-            <Typography variant="h5" className="text-center text-white">
-              Victime d'harcèlement ?
-            </Typography>
-          </CardContent>
-          <CardActions className="flex justify-center">
-            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
-          </CardActions>
-        </Card>
-
-        <Card className="mt-5 mb-5 bg-gradient-to-r from-indigo-300 to-indigo-700">
-          <CardContent>
-            <Typography variant="h5" className="text-center text-white">
-            Témoin d'harcèlement ?
-            </Typography>
-          </CardContent>
-          <CardActions className="flex justify-center">
-            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
-          </CardActions>
-        </Card>
-
-        <Card className="mt-5 mb-5 bg-gradient-to-r from-sky-300 to-sky-700">
-          <CardContent>
-            <Typography variant="h5" className="text-center text-white">
-            Fatigué de votre travail ?
-            </Typography>
-          </CardContent>
-          <CardActions className="flex justify-center">
-            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
-          </CardActions>
-        </Card>
+    {forms.map((form: Form) =>
+    ( <Card className={`mt-5 mb-5 bg-gradient-to-r ${form.type === FormType.HARASSMENT ? `from-blue-300 to-blue-700` : form.type === FormType.TESTIMONY ? `from-indigo-300 to-indigo-700` : form.type === FormType.BURN_OUT ? `from-sky-300 to-sky-700` : form.type === FormType.MOTIVOMETER ? `from-cyan-300 to-cyan-700` : ''}`}>
+        <CardContent>
+          <Typography variant="h5" className="text-center text-white">
+            {form.type === FormType.HARASSMENT ? `Victime d'harcèlement ?` : form.type === FormType.TESTIMONY ? `Témoin d'harcèlement ?` : form.type === FormType.BURN_OUT ? `Fatigué de votre travail ?` : form.type === FormType.MOTIVOMETER ? `Motivomètre` : ''}
+          </Typography>
+        </CardContent>
+        <CardActions className="flex justify-center">
+          <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
+        </CardActions>
+      </Card>
+  ))}
   </Container>
   )
 }
