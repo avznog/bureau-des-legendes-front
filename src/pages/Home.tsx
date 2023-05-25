93dda8adb9b8b119250d9d618751c4fa003fd527
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2023-05-25 16:24:59 +0200

    form of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,46 @@
-import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
+import { Button, Card, CardActions, CardContent, Container, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
 
 export function Home() {
-  const auth = useAuthUser();
-  const isAuthenticated = useIsAuthenticated();
+  useEffect(() => {
+    axios.get(`http://localhost:8000/forms/team/1`).then((response) => console.log(response.data)).catch((error) => console.log(error));
+  })
   
-  return (<div>
-    <p>{isAuthenticated() ? 
-    'Logged' : 'Not logged'}</p>
-    <p>Bienvenue {auth()?.user}</p>
-  </div>)
+  return (
+  <Container>
+        <Card className="mt-5 mb-5 bg-gradient-to-r from-blue-300 to-blue-700">
+          <CardContent>
+            <Typography variant="h5" className="text-center text-white">
+              Victime d'harcèlement ?
+            </Typography>
+          </CardContent>
+          <CardActions className="flex justify-center">
+            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="mt-5 mb-5 bg-gradient-to-r from-indigo-300 to-indigo-700">
+          <CardContent>
+            <Typography variant="h5" className="text-center text-white">
+            Témoin d'harcèlement ?
+            </Typography>
+          </CardContent>
+          <CardActions className="flex justify-center">
+            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="mt-5 mb-5 bg-gradient-to-r from-sky-300 to-sky-700">
+          <CardContent>
+            <Typography variant="h5" className="text-center text-white">
+            Fatigué de votre travail ?
+            </Typography>
+          </CardContent>
+          <CardActions className="flex justify-center">
+            <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
+          </CardActions>
+        </Card>
+  </Container>
+  )
 }
