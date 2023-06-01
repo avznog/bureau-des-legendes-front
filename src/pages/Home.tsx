d48702c5041ee132c01d6e776d735c878edf09de
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-06-01 13:59:29 +0200

    🐛 fix dumb bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,19 @@ import { useAuthUser } from "react-auth-kit";
 import { FormType } from "../constants/form.type";
 import { Form } from "../models/form.model";
 import axios from "../axios/axios";
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
   const auth = useAuthUser();
   const [forms, setForms] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
-    axios.get(`forms/team/${auth()?.person.person.team.id}`).then((response) => setForms(response.data)).catch((error) => console.log(error));
+    auth()?.person.person.team && axios.get(`forms/team/${auth()?.person.person.team.id}`).then((response) => setForms(response.data)).catch((error) => console.log(error));
   }, [setForms])
     
   return (
-  <Container>
-    {forms.map((form: Form) =>
+  <div className="w-full">
+    {auth()?.person.person.team ? forms.map((form: Form) =>
     ( <Card className={`mt-5 mb-5 bg-gradient-to-r ${form.type === FormType.HARASSMENT ? `from-blue-300 to-blue-700` : form.type === FormType.TESTIMONY ? `from-indigo-300 to-indigo-700` : form.type === FormType.BURN_OUT ? `from-sky-300 to-sky-700` : form.type === FormType.MOTIVOMETER ? `from-cyan-300 to-cyan-700` : ''}`}>
         <CardContent>
           <Typography variant="h5" className="text-center text-white">
@@ -27,7 +28,15 @@ export function Home() {
           <Button size="medium" color="primary" variant="contained" className="text-center">Complétez ce formulaire</Button>
         </CardActions>
       </Card>
-  ))}
-  </Container>
+  )) 
+  :
+  <div className="w-full">
+    <div className="flex justify-center w-full p-5">Vous devez d'abord chosir une équipe :</div>
+    <div className="flex justify-center w-full">
+      <Button className="justify-center" variant="contained" onClick={() => navigate('/team')}>Choisir une équipe</Button>
+    </div>
+  </div> 
+  }
+  </div>
   )
 }
